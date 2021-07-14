72c753256302197592bbbcc9895d9e75e5ee1bff
mrakash01 <87415976+mrakash01@users.noreply.github.com>
2021-07-14 11:27:26 +0000

    Add files via upload

---
+++ login2.js
@@ -0,0 +1,46 @@
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if(username=="pornhub"&& password=="miakhalifa")
+{
+    alert("Login Successfully!! ")
+    window.location.href = 'zoomraids.html';
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+else
+{
+    alert("Fuck You!!  ਸਾਲੇਆ Password ਨਹੀ ਪਤਾ ਤਾਂ ਆਪਣੇ  ਬੂੜੇ ਨਾਲ ਬੁੰਡ ਪੰਗੇ ਨਾ ਲੈ")
+    window.location = 'index.html';
+}
+
+
+}
+
+function bumb()
+{
+    alert("ਉਸਤਾਦ ਜੇ ਤੈਨੂੰ Password ਪਤਾ ਆ ਤਾ ਹੀ Login  ਕਰੀ| ਬਾਅਦ ਵਿੱਚ ਮੈਨੂੰ ਨਾ ਕਈ|")
+}
