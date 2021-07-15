bbe4e55854b2c78f3c3c0a7d2a26d20e0c53946d
mrakash01 <87415976+mrakash01@users.noreply.github.com>
2021-07-15 13:36:33 +0530

    Add files via upload

---
+++ login2.js
@@ -0,0 +1,64 @@
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if(username=="pornhub"&& password=="miakhalifa") {
+
+    
+
+    alert("Read The Password:     ilovemiakhalifa")
+    alert("Listen Carefully")
+    alert("Now, IN the Next Website ")
+    alert("There is an error")
+    alert("Don't Worry")
+    alert("Copy The Website Url NOT THIS (The Next One)")
+    alert("And Paste It Again ")
+    alert("Now It Works")
+
+    return true;
+    
+
+}
+alert("Fuck You!!  ਸਾਲੇਆ Password ਨਹੀ ਪਤਾ ਤਾਂ ਆਪਣੇ  ਬੂੜੇ ਨਾਲ ਬੁੰਡ ਪੰਗੇ ਨਾ ਲੈ");
+alert("@@ Haramdaya @@")
+alert("@@ Kutaya @@")
+alert("@@ Kaminaya @@")
+alert("@@ Bondoya")
+alert("@@ Hun Na Panga layi")
+
+window.location.href = 'https://pbs.twimg.com/profile_images/813595489299873792/n5au0DF7.jpg'
+    return false;
+
+
+
+
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function bumb()
+{
+    alert("ਉਸਤਾਦ ਜੇ ਤੈਨੂੰ Password ਪਤਾ ਆ ਤਾ ਹੀ Login  ਕਰੀ| ਬਾਅਦ ਵਿੱਚ ਮੈਨੂੰ ਨਾ ਕਈ|")
+}
