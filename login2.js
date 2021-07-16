a3a3c93a5c6469c23637cb6979300392dbcb4a60
mrakash01 <87415976+mrakash01@users.noreply.github.com>
2021-07-16 18:07:25 +0530

    Add files via upload

---
+++ login2.js
@@ -0,0 +1,62 @@
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if(username=="pornhub"&& password=="miakhalifa") {
+
+    
+    https://www.google.com/url?sa=i&url=https%3A%2F%2Fin.pinterest.com%2Fak88vi%2Fgali-memes%2F&psig=AOvVaw2pWr1CcmdIDtgTVXdCmSkZ&ust=1626522624790000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCPin15jD5_ECFQAAAAAdAAAAABAD
+    
+    alert("You Are Successfully Login")
+    
+
+    return true;
+    
+
+}
+alert("Fuck You!!  ਸਾਲੇਆ Password ਨਹੀ ਪਤਾ ਤਾਂ ਆਪਣੇ  ਬੂੜੇ ਨਾਲ ਬੁੰਡ ਪੰਗੇ ਨਾ ਲੈ");
+alert("@@ Haramdaya @@")
+window.location.href = '  https://lh3.googleusercontent.com/proxy/4tX5yWQ3icnPEj_zGfO2-WQzvEDTLcsWC6OSvzcts4YafrWrVp3MgVphnti7O6zSRpZQcEOGOHsBP6tkq2VIbUg_wz0gsjlhmrFZ6r0R1k2nYV4g3oOJ03fn5DZeRwqefu4q3hPW=s0-d'
+
+alert("@@ Kutaya @@")
+
+alert("@@ Kaminaya @@")
+alert("@@ Bondoya")
+alert("@@ Hun Na Panga layi")
+
+window.location.href = 'https://pbs.twimg.com/profile_images/813595489299873792/n5au0DF7.jpg'
+    return false;
+
+
+
+
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function bumb()
+{
+    alert("ਉਸਤਾਦ ਜੇ ਤੈਨੂੰ Password ਪਤਾ ਆ ਤਾ ਹੀ Login  ਕਰੀ| ਬਾਅਦ ਵਿੱਚ ਮੈਨੂੰ ਨਾ ਕਈ|")
+}
